099012cce3b6ef9d64e982b3027200e9fe1dacbc
Carter Dobbs <cooldogyum@gmail.com>
2021-08-31 23:01:32 -0500

    init

---
+++ script.js
@@ -0,0 +1,85 @@
+const form = document.querySelector('#equation-form');
+const input = document.querySelector('#equation');
+const results = document.querySelector('#results');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const equation = input.value;
+    const result = parseEquation(equation);
+    results.textContent = result;
+});
+
+input.addEventListener('keypress', e => {
+    const invalidKey = new RegExp(/[^()^*\/+-\d.\s]/g).test(e.key);
+    if (invalidKey) e.preventDefault();
+});
+
+// FIXME: Fix these regex's
+// 2^3*-1 = -8
+// 4*(2+3-1) = 16
+// 2^3*5 = 40
+const PARENTHESIS_REGEX = /\((?<equation>[^\(\)]*)\)/;
+const EXPONENT_REGEX = /(?<operand1>\S+)\s*(?<operator>\^)\s*(?<operand2>\S+)/;
+const MULTIPLY_DIVIDE_REGEX = /(?<operand1>\S+)\s*(?<operator>[\*\/])\s*(?<operand2>\S+)/;
+const ADD_SUBTRACT_REGEX = /(?<operand1>\S+)\s*(?<operator>(?<!e)[\+\-])\s*(?<operand2>\S+)/;
+
+// const hasParenthesis = new RegExp(/\([()^*/+\-.\d]+\)/g).test(equation);
+// console.log(hasParenthesis)
+
+function parseEquation(equation) {
+    // Parenthesis
+    const parenthesis = equation.match(PARENTHESIS_REGEX);
+    if (parenthesis) {
+        const subEquation = parenthesis.groups.equation;
+        const result = parseEquation(subEquation);
+        const newEquation = equation.replace(PARENTHESIS_REGEX, result);
+        return parseEquation(newEquation);
+    }
+
+    // Exponents
+    const exponent = equation.match(EXPONENT_REGEX);
+    if (exponent) {
+        const result = handleOperation(exponent.groups);
+        const newEquation = equation.replace(EXPONENT_REGEX, result);
+        return parseEquation(newEquation);
+    }
+
+    // Multiplication / Division
+    const multiplyDivide = equation.match(MULTIPLY_DIVIDE_REGEX);
+    if (multiplyDivide) {
+        const result = handleOperation(multiplyDivide.groups);
+        const newEquation = equation.replace(MULTIPLY_DIVIDE_REGEX, result);
+        return parseEquation(newEquation);
+    }
+
+    // Addition / Subtraction
+    const addSubtract = equation.match(ADD_SUBTRACT_REGEX);
+    if (addSubtract) {
+        const result = handleOperation(addSubtract.groups);
+        const newEquation = equation.replace(ADD_SUBTRACT_REGEX, result);
+        return parseEquation(newEquation);
+    }
+
+    return parseFloat(equation);
+}
+
+function handleOperation({ operand1, operand2, operator }) {
+    const number1 = parseFloat(operand1);
+    const number2 = parseFloat(operand2);
+
+    switch (operator) {
+        case '^':
+            return number1 ** number2;
+        case '*':
+            return number1 * number2;
+        case '/':
+            return number1 / number2;
+        case '+':
+            return number1 + number2;
+        case '-':
+            return number1 - number2;
+        default:
+            console.error(`Unknown operator: ${operator}`);
+            break;
+    }
+}
